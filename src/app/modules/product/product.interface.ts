3924be9f3b0e86dbb3564f74d43417a4000763ac
utsho11 <utshoroy521@gmail.com>
2024-05-21 19:39:06 +0600

    product interface added

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,26 @@
+import { Model } from 'mongoose';
+
+// 1. Create an interface representing a document in MongoDB.
+
+export type TVariants = {
+  type: string;
+  value: string;
+};
+
+export type TInventory = {
+  quantity: number;
+  inStock: boolean;
+};
+
+export type TProduct = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: TVariants[];
+  inventory: TInventory;
+  productImg?: string;
+};
+
+export interface ProductModel extends Model<TProduct> {};
